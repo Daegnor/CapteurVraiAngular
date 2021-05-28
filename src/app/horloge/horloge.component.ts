7649005e36c9395054855b553e47e6742ded18d0
Daegnor <sebastien.prunier@etu.univ-lehavre.fr>
2021-05-28 14:43:13 +0200

    Déplacement de l'alerte vers horlogeGraph

--- src/app/horloge/horloge.component.ts
+++ src/app/horloge/horloge.component.ts
@@ -14,52 +14,13 @@ import {HorlogeGraphComponent} from '../horlogeGraph/horlogeGraph.component';
  * Affiche les valeurs du capteurs à CO2/Température
  * Hérite de HorlogeGraphComponent
  */
-export class HorlogeComponent extends HorlogeGraphComponent implements OnDestroy {
-	intervalAlerte = null;
-	// Constante définissant le déclenchement de l'alarme
-	NIVEAU_ALERTE = 800;
+export class HorlogeComponent extends HorlogeGraphComponent {
 
 	constructor(protected actRoute: ActivatedRoute, protected router: Router, protected http: HttpClient) {
 		super(actRoute, router, http);
 	}
 
-	setNiveau(): void {
-		super.setNiveau();
-
-		// Initialise l'alarme si la valeur de CO2 est supérieure à NIVEAU_ALERTE, ou l'arrête si inférieur
-		if (!this.niveauCapteurs.hasOwnProperty('co2')) {
-			return;
-		}
-
-		const niveauCO2 = this.niveauCapteurs.co2;
-
-		if (niveauCO2 >= this.NIVEAU_ALERTE && this.intervalAlerte === null) {
-			this.alerte();
-			this.intervalAlerte = setInterval(this.alerte, 10000);
-		} else if (this.intervalAlerte !== null && niveauCO2 < this.NIVEAU_ALERTE) {
-			clearInterval(this.intervalAlerte);
-			this.intervalAlerte = null;
-		}
-	}
-
-	/**
-	 * Méthode effectuant une alerte (passage du fond en rouge, puis de nouveau en noir au bout d'une seconde)
-	 */
-	alerte(): void {
-		$('.main_container').css('background-color', 'red');
-		setTimeout(() => {
-			$('.main_container').css('background-color', 'black');
-		}, 1000);
-	}
-
 	getGraphData(): void {
 		return;
-	}
-
-	ngOnDestroy(): void {
-		super.ngOnDestroy();
-		if (this.intervalAlerte) {
-			clearInterval(this.intervalAlerte);
-		}
 	}
 }
--- src/app/horlogeGraph/horlogeGraph.component.ts
+++ src/app/horlogeGraph/horlogeGraph.component.ts
@@ -1,4 +1,4 @@
-import {Component, forwardRef, OnInit} from '@angular/core';
+import {Component, forwardRef, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {PowerComponent} from '../power/power.component';
 import {ActivatedRoute, Router} from '@angular/router';
@@ -15,7 +15,11 @@ import {ActivatedRoute, Router} from '@angular/router';
  * Affiche les valeurs du capteurs à CO2 ainsi que son évolution sur le temps
  * Hérite de PowerComponent
  */
-export class HorlogeGraphComponent extends PowerComponent implements OnInit {
+export class HorlogeGraphComponent extends PowerComponent implements OnInit, OnDestroy {
+	intervalAlerte = null;
+	// Constante définissant le déclenchement de l'alarme
+	NIVEAU_ALERTE = 800;
+
 	constructor(protected actRoute: ActivatedRoute, protected router: Router, protected http: HttpClient) {
 		super(actRoute, router, http);
 		this.id = 'horloge';
@@ -24,8 +28,44 @@ export class HorlogeGraphComponent extends PowerComponent implements OnInit {
 		this.beginAtZero = true;
 	}
 
+	setNiveau(): void {
+		super.setNiveau();
+
+		// Initialise l'alarme si la valeur de CO2 est supérieure à NIVEAU_ALERTE, ou l'arrête si inférieur
+		if (!this.niveauCapteurs.hasOwnProperty('co2')) {
+			return;
+		}
+
+		const niveauCO2 = this.niveauCapteurs.co2;
+
+		if (niveauCO2 >= this.NIVEAU_ALERTE && this.intervalAlerte === null) {
+			this.alerte();
+			this.intervalAlerte = setInterval(this.alerte, 10000);
+		} else if (this.intervalAlerte !== null && niveauCO2 < this.NIVEAU_ALERTE) {
+			clearInterval(this.intervalAlerte);
+			this.intervalAlerte = null;
+		}
+	}
+
+	/**
+	 * Méthode effectuant une alerte (passage du fond en rouge, puis de nouveau en noir au bout d'une seconde)
+	 */
+	alerte(): void {
+		$('.main_container').css('background-color', 'red');
+		setTimeout(() => {
+			$('.main_container').css('background-color', 'black');
+		}, 1000);
+	}
+
 	ngOnInit(): void {
 		super.ngOnInit();
 		$('#nomCapteur').text('Horloge à CO2');
+	}
+
+	ngOnDestroy(): void {
+		super.ngOnDestroy();
+		if (this.intervalAlerte) {
+			clearInterval(this.intervalAlerte);
+		}
 	}
 }
